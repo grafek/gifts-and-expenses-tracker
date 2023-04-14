fbb8ba1e69e4043529c92391e0e38315dfadd5ca
grafek <jacek.grafender@gmail.com>
2023-04-15 00:15:16 +0200

    chore: chart fixes and better UX when adding an expense

--- src/components/Expenses.client.tsx
+++ src/components/Expenses.client.tsx
@@ -1,36 +1,50 @@
 "use client";
-import { type Expense } from "@/types";
+
 import { MONTHS, YEARS } from "@/globals";
-import React, { Dispatch, SetStateAction, use, useMemo, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import Input from "./Input.client";
 import Button from "./Button.client";
 import Select from "./Select.client";
 import withAuth from "@/hoc/withAuth";
-import { ChartSkeleton, Spinner } from "./Loaders.server";
+import { Spinner } from "./Loaders.server";
 import { addExpense, auth, getExpenses } from "@/lib/firebase";
+import { type Expense } from "@/types";
 
 const ExpensesChart: React.FC = () => {
   const [selectedMonth, setSelectedMonth] = useState(
     new Date().toLocaleString("default", { month: "long" })
   );
-  const [selectedYear, setSelectedYear] = useState(new Date().getFullYear());
-
-  const [isAdded, setIsAdded] = useState(false);
+  const [selectedYear, setSelectedYear] = useState(
+    new Date().getFullYear().toString()
+  );
+  const [expenses, setExpenses] = useState<Expense[]>([]);
 
   const [showForm, setShowForm] = useState(false);
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const promise = useMemo(() => getExpenses(auth.currentUser?.uid!), [isAdded]);
+  const handleAddExpense = useCallback(async (expense: Expense) => {
+    await addExpense(auth.currentUser?.uid!, expense);
 
-  const expenses = use(promise);
+    const updatedExpenses = await getExpenses(auth.currentUser?.uid!);
+    setExpenses(updatedExpenses);
+    setShowForm(false);
+  }, []);
+
+  const fetchExpenses = useCallback(async () => {
+    const expensesData = await getExpenses(auth.currentUser?.uid!);
+    setExpenses(expensesData);
+  }, []);
+
+  useEffect(() => {
+    fetchExpenses();
+  }, [fetchExpenses]);
 
   const filteredExpenses = expenses.filter((expense) => {
     if (selectedYear && selectedMonth) {
-      return expense.year === selectedYear && expense.month === selectedMonth;
+      return expense.year == selectedYear && expense.month == selectedMonth;
     } else if (selectedYear) {
-      return expense.year === selectedYear;
+      return expense.year == selectedYear;
     } else if (selectedMonth) {
-      return expense.month === selectedMonth;
+      return expense.month == selectedMonth;
     } else {
       return true;
     }
@@ -45,7 +59,8 @@ const ExpensesChart: React.FC = () => {
     0
   );
 
-  if (!expenses) return <ChartSkeleton />;
+  const CHART_HEIGHT = 567;
+  const BAR_WIDTH = 40;
 
   return (
     <>
@@ -59,8 +74,8 @@ const ExpensesChart: React.FC = () => {
           Add Expense
         </Button>
       </div>
-      {showForm ? <ExpenseForm setIsAdded={setIsAdded} /> : null}
-      <div className="mx-auto mt-4 min-h-full max-w-3xl rounded-lg bg-black shadow-md">
+      {showForm ? <ExpenseForm onHandleAdd={handleAddExpense} /> : null}
+      <div className="mx-auto mt-4 h-[90%] max-w-6xl rounded-lg bg-black shadow-md">
         <div className="flex items-center justify-between border-b border-gray-700 p-4">
           <h3 className="font-medium ">Total Expenses: ${totalExpenses}</h3>
           <div className="flex flex-col gap-3 sm:flex-row sm:gap-5">
@@ -68,7 +83,7 @@ const ExpensesChart: React.FC = () => {
               options={YEARS}
               value={selectedYear}
               onChange={async (e) => {
-                setSelectedYear(Number(e.target.value));
+                setSelectedYear(e.target.value);
               }}
             />
             <Select
@@ -80,16 +95,21 @@ const ExpensesChart: React.FC = () => {
         </div>
         <div className="overflow-x-auto">
           <svg
-            className={`min-w-full w-${expenses.length * 50 + 10}`}
-            height={"420"}
+            className={`min-w-full`}
+            width={`${expenses.length * 60 + 10}`}
+            height={CHART_HEIGHT}
           >
             {filteredExpenses.map((data, index) => (
               <rect
                 key={data.id}
-                x={index * 50 + 20}
-                y={400 - (data.value / maxAmount) * 380}
-                height={(data.value / maxAmount) * 380}
-                className={`w-7 ${
+                x={index * 60 + 20}
+                y={
+                  CHART_HEIGHT -
+                  20 -
+                  (data.value / maxAmount) * (CHART_HEIGHT - 50)
+                }
+                height={(data.value / maxAmount) * (CHART_HEIGHT - 50)}
+                className={`w-10 ${
                   data.value === maxAmount ? "fill-red-700" : "fill-blue-700"
                 }`}
                 rx="5"
@@ -100,8 +120,13 @@ const ExpensesChart: React.FC = () => {
             {filteredExpenses.map((data, index) => (
               <text
                 key={data.id}
-                x={index * 50 + 35}
-                y={400 - (data.value / maxAmount) * 380 - 8}
+                x={index * 60 + BAR_WIDTH}
+                y={
+                  CHART_HEIGHT -
+                  20 -
+                  (data.value / maxAmount) * (CHART_HEIGHT - 50) -
+                  8
+                }
                 className={`fill-[#bbb] text-[12px]`}
                 textAnchor="middle"
               >
@@ -111,9 +136,9 @@ const ExpensesChart: React.FC = () => {
             {filteredExpenses.map((data, index) => (
               <text
                 key={data.id}
-                x={index * 50 + 35}
-                y={414}
-                className={`fill-[#bbb] text-[11px] tracking-wider`}
+                x={index * 60 + BAR_WIDTH}
+                y={CHART_HEIGHT - 4}
+                className={`fill-[#f5f5f5] text-[12px] font-semibold tracking-wider`}
                 textAnchor="middle"
               >
                 {`$${data.value}`}
@@ -130,49 +155,43 @@ ExpensesChart.displayName = "Expenses";
 export default withAuth(ExpensesChart);
 
 type ExpenseFormProps = {
-  setIsAdded: Dispatch<SetStateAction<boolean>>;
+  onHandleAdd: (expense: Expense) => Promise<void>;
 };
 
-const ExpenseForm: React.FC<ExpenseFormProps> = ({ setIsAdded }) => {
-  const [name, setName] = useState("");
-  const [value, setValue] = useState("");
-  const [month, setMonth] = useState(
-    new Date().toLocaleString("default", { month: "long" })
-  );
-  const [year, setYear] = useState(new Date().getFullYear());
+const INITIAL_EXPENSE = {
+  name: "",
+  value: "" as unknown as number,
+  month: new Date().toLocaleString("default", { month: "long" }),
+  year: new Date().getFullYear().toString(),
+};
+
+const ExpenseForm: React.FC<ExpenseFormProps> = ({ onHandleAdd }) => {
+  const [expense, setExpense] = useState<Expense>(INITIAL_EXPENSE);
 
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
 
-  const handleSubmit = async (event: any) => {
-    setIsAdded(false);
-    setError(null);
-    event.preventDefault();
-    if (name.length < 1) {
-      setError("Name cannot be empty!");
-      return;
-    } else if (value.length < 1 || Number(value) < 1) {
-      setError("Value cannot be empty or =<0!");
-      return;
-    }
-    setLoading(true);
+  const handleSubmit = useCallback(
+    async (event: any) => {
+      event.preventDefault();
 
-    const error = await addExpense(auth.currentUser?.uid!, {
-      name,
-      value: Number(value),
-      month: month as Expense["month"],
-      year: Number(year),
-    });
+      setError(null);
 
-    setLoading(false);
-    if (error) {
-      setError(error);
-      return;
-    }
-    setName("");
-    setValue("");
-    setIsAdded(true);
-  };
+      if (expense.name.length < 1) {
+        setError("Name cannot be empty!");
+        return;
+      } else if (Number(expense.value) < 1) {
+        setError("Value cannot be less than a 0!");
+        return;
+      }
+      setLoading(true);
+
+      await onHandleAdd(expense);
+
+      setExpense(INITIAL_EXPENSE);
+    },
+    [expense, onHandleAdd]
+  );
 
   return (
     <form
@@ -184,11 +203,11 @@ const ExpenseForm: React.FC<ExpenseFormProps> = ({ setIsAdded }) => {
           required
           type="text"
           name="name"
-          value={name}
+          value={expense.name}
           error={error}
           labelname="Name"
           onChange={(e) => {
-            setName(e.target.value);
+            setExpense({ ...expense, name: e.target.value });
             setError(null);
           }}
         />
@@ -200,26 +219,25 @@ const ExpenseForm: React.FC<ExpenseFormProps> = ({ setIsAdded }) => {
           name="value"
           labelname="Value"
           error={error}
-          value={value}
+          value={expense.value}
           onChange={(e) => {
-            setValue(e.target.value);
+            setExpense({ ...expense, value: Number(e.target.value) });
             setError(null);
           }}
         />
       </div>
-
       <div className="relative">
         <Select
           options={MONTHS}
-          value={month}
-          onChange={(e) => setMonth(e.target.value as Expense["month"])}
+          value={expense.month}
+          onChange={(e) => setExpense({ ...expense, month: e.target.value })}
         />
       </div>
       <div className="relative">
         <Select
           options={YEARS}
-          value={year}
-          onChange={(e) => setYear(Number(e.target.value))}
+          value={expense.year}
+          onChange={(e) => setExpense({ ...expense, year: e.target.value })}
         />
       </div>
       <Button
